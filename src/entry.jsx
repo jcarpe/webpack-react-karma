4b115476134d1fd6939d106846be468231f56d2d
jcarpe <joe_carpenito@yahoo.com>
2016-06-05 16:01:13 -0400

    remove old styles.scss reference

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,9 +1,6 @@
-// require( './styles.css' );
-// document.write( require( './content.js' ) );
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './styles.scss';
 import World from './content.jsx';
 
 ReactDOM.render( <World />, document.getElementById('app') );
